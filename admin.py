cd73ba001278d646b3c49f12a240b6d69867f802
Your Name <dotwanazenande@gmail.com>
2021-01-22 16:18:12 +0200

    second push

--- admin.py
+++ admin.py
@@ -2,23 +2,37 @@ from tkinter import *
 import mysql.connector
 mydb = mysql.connector.connect(user="lifechoices",password="@Lifechoices314",database="lifechoicesonline",
                                host="127.0.0.1",auth_plugin="mysql_native_password")
-mycursor = mydb.cursor()
+
 
 window = Tk()
 window.title("Admin")
 window.geometry("500x500")
 window.config(bg="green")
 
-my_listbox = Listbox(window , width="50")
-my_listbox.pack(pady = 15)
 
-my_listbox.insert(END, "This is a second item")
-my_listbox.insert(END, "second item")
+my_conn = mydb.cursor()
+my_conn.execute("SELECT full_name FROM users limit 0,5")
+i=0
+for users in my_conn:
+    for j in range(len(users)):
+        my_listbox = Listbox(window, width="50",height="1")
+        my_listbox.pack()
+        my_listbox.insert(END , users[j])
+        i=i+1
 
-my_list = ["one" , "two" , "three","one" , "two" , "three"]
 
-for item in my_list:
-    my_listbox.insert(END , item)
+
+
+# my_listbox = Listbox(window , width="50")
+# my_listbox.pack(pady = 15)
+
+# my_listbox.insert(END, "This is a second item")
+# my_listbox.insert(END, "second item")
+
+# my_list = ["one" , "two" , "three","one" , "two" , "three"]
+
+# for item in my_list:
+#     my_listbox.insert(END , item)
 
 def delete():
     my_listbox.delete(ANCHOR)
@@ -31,17 +45,17 @@ def delete_all():
     my_listbox.delete(0 , END)
 
 my_button = Button(window , text = "Delete" ,bg="black",fg="white", command =delete)
-my_button.pack(pady = 10)
+my_button.pack()
 
-my_button2 = Button(window , text = "select" , bg="black",fg="white",command =select)
-my_button2.pack(pady = 10)
+my_button2 = Button(window , text = "Add User" , bg="black",fg="white",command =select)
+my_button2.pack()
 
-my_button3 = Button(window , text = "delete all" ,bg=" black",fg="white", command =delete_all)
-my_button3.pack(pady = 10)
+my_button3 = Button(window , text = "delete all" ,bg="black",fg="white", command =delete_all)
+my_button3.pack()
 
 global my_label
 my_label = Label(window , text="")
-my_label.pack(pady = 5)
+my_label.pack()
 
 
 
--- logs.py
+++ logs.py
@@ -1,62 +1,122 @@
 from tkinter import *
 from tkinter import messagebox
 import mysql.connector
+
+
+
+
 mydb = mysql.connector.connect(user="lifechoices",password="@Lifechoices314",database="lifechoicesonline",
                                host="127.0.0.1",auth_plugin="mysql_native_password")
 mycursor = mydb.cursor()
-
 def LOGS():
     user=userEntry.get()
     passs= passEntry.get()
-    sql='select * from LOGIN_OUT WHERE username = %s and password = %s'
+    sql='select * from users WHERE username = %s and password = %s'
     mycursor.execute(sql, [(user), (passs)])
     results = mycursor.fetchall()
     if results:
         for i in results:
             logged()
-            messagebox.showinfo("Log in successful!")
             break
     else:
         failed()
 
 def logged():
-    messagebox.showinfo("Log in successful!")
+    messagebox.showinfo("LOG IN MESSAGE","Log in successful!")
+    import mobile
 
 def failed():
     messagebox.showinfo("login unsuccessful!")
     userEntry.delete(0,END)
     passEntry.delete(0,END)
     window.withdraw()
-    import reg
-    reg.status()
+
+
+
+def register():
+    def insertdb():
+        id1 = idEntry.get()
+        fullname=nameEntry.get()
+        user = userEntry.get()
+        passs = passEntry.get()
+        mydb = mysql.connector.connect(user="lifechoices", password="@Lifechoices314", database="lifechoicesonline",
+                                       host="127.0.0.1", auth_plugin="mysql_native_password")
+        mycursor = mydb.cursor()
+        sql = 'INSERT INTO users(id , full_name,username,password) VALUES(%s,%s,%s,%s)'
+        val = (id1,fullname,user,passs)
+        mycursor.execute(sql,val)
+        mydb.commit()
+        messagebox.showinfo("REGISTRY","Registration successful")
+
+        print(mycursor.rowcount,"inserted")
+
+    print("test")
+    # get all values from the entries.
+    # Write INSERT sql state to add values in the database
+    # Display message confirming user registration
+    window = Tk()
+    window.title("Registering A User!")
+    window.geometry("500x500")
+    window.config(bg="green")
+
+
+    myLabel = Label(window,text="REGISTER!")
+    myLabel.pack()
+
+    idlabel = Label(window , text="id")
+    idlabel.pack()
+    idEntry = Entry(window , textvariable = "id" )
+    idEntry.pack()
+
+    namelabel = Label(window , text="full name")
+    namelabel.pack()
+    nameEntry = Entry(window , textvariable = "name" )
+    nameEntry.pack()
+
+    userlabel = Label(window, text="username")
+    userlabel.pack()
+    userEntry = Entry(window, textvariable="username")
+    userEntry.pack()
+
+    passlabel = Label(window, text="password")
+    passlabel.pack()
+    passEntry = Entry(window, textvariable="password")
+    passEntry.pack()
+
+    regButton = Button(window, text="Register" ,command=insertdb)
+    regButton.pack()
+
+    window.mainloop()
+
+
+
+
+
 
 window  = Tk()
 window.geometry("500x500")
 window.title("LOGIN")
-window.config(bg="white")
+window.config(bg="green")
 photo = PhotoImage(file = "index.png")
 w = Label(window , image = photo)
 w.pack()
 
 
-
-
-
 userlabel = Label(window , text="username")
 userlabel.pack()
-userEntry = Entry(window , textvariable = "username" ,fg="black")
+userEntry = Entry(window , textvariable = "username")
 userEntry.pack()
 
 passlabel = Label(window , text="password")
 passlabel.pack()
-passEntry = Entry(window , textvariable = "password" ,fg="black")
+passEntry = Entry(window , textvariable = "password" )
 passEntry.pack()
 
-logButton = Button(window , text="Login", command=LOGS)
+logButton = Button(window , text="Login",bg="black",fg="white", command=LOGS)
 logButton.pack()
 
 messlabel = Label(window , text="new at lifechoices?")
 messlabel.pack()
-regButton = Button(window , text="Register", command=LOGS)
+regButton = Button(window , text="Admin", bg="black",fg="white",command=register)
 regButton.pack()
 window.mainloop()
